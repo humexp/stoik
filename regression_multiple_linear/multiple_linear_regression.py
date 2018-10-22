0e99c8d617a91e76700a4a84024418c76985bb17
MarkKlimenko <22773@bk.ru>
2018-10-23 04:19:10 +1000

    Implement Backward Elimination

--- regression_multiple_linear/multiple_linear_regression.py
+++ regression_multiple_linear/multiple_linear_regression.py
@@ -26,6 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Fitting Multiple linear regression for the training set
 from sklearn.linear_model import LinearRegression
+
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
@@ -34,4 +35,24 @@ y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
 import statsmodels.formula.api as sm
+
 X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
